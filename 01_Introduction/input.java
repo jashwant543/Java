4fda6e1ac56b3eb014160709692ef22f709c37a5
Jashwant Kumar <singh943235humpty@gmail.com>
2025-08-17 18:32:47 +0530

    inputs

---
+++ 01_Introduction/input.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class input {
+    
+    public static  void main(String[] args)
+    {
+        //get input as string
+        Scanner scan = new Scanner(System.in);
+       
+        int n = scan.nextInt();
+        System.out.print(n +"\n");
+        //get intgere 
+        scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        System.out.print(s);
+
+
+
+
+    }
+}
